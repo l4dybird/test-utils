cba909afd74c169e7e5367260dc12b655c997fbb
l4dybird <future.l4dy61rd@gmail.com>
2024-06-04 22:02:26 +0900

    test

--- examples/app-vitest-full/tests/nuxt/config.spec.ts
+++ examples/app-vitest-full/tests/nuxt/config.spec.ts
@@ -4,7 +4,7 @@ it('should return the runtimeConfig from nuxt.config', () => {
   const config = useRuntimeConfig()
   expect(config).toBeTypeOf('object')
   expect(config?.public).toEqual({
-    hello: 'world',
+    hello: 'nuxt',
     testValue: 123,
   })
 })
