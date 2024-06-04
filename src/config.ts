ba8009b351f52947f5890e07e10e9eb0e12fa0eb
l4dybird <future.l4dy61rd@gmail.com>
2024-06-04 21:30:14 +0900

    aaaaaaa

--- src/config.ts
+++ src/config.ts
@@ -198,7 +198,7 @@ export async function getVitestConfigFromNuxt(
 
 export function defineVitestConfig(config: InlineConfig & { test?: VitestConfig } = {}) {
   // @ts-expect-error TODO: investigate type mismatch
-  return mergeConfig({}, async () => {
+  return defineConfig(() => mergeConfig({}, async () => {
     // When Nuxt module calls `startVitest`, we don't need to call `getVitestConfigFromNuxt` again
     if (process.env.__NUXT_VITEST_RESOLVED__) return config
 
@@ -222,7 +222,7 @@ export function defineVitestConfig(config: InlineConfig & { test?: VitestConfig 
 
     console.log('marge', mergeConfig.test?.environmentOptions?.nuxt?.overrides)
     return mergeConfig
-  })
+  }))
 
   // @ts-expect-error TODO: investigate type mismatch
   return defineConfig(async () => {
