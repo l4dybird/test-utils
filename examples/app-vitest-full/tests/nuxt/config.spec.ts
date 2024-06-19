74a7cb70db7ae95eb7d189048a79aa732bc2ee34
l4dybird <future.l4dy61rd@gmail.com>
2024-06-19 23:59:11 +0900

    aaaaaaaa

--- examples/app-vitest-full/tests/nuxt/config.spec.ts
+++ examples/app-vitest-full/tests/nuxt/config.spec.ts
@@ -3,6 +3,8 @@ import { expect, it } from 'vitest'
 it('should return the runtimeConfig from nuxt.config', () => {
   const config = useRuntimeConfig()
   expect(config).toBeTypeOf('object')
+  console.log('check:object', config?.public)
+  console.log('check:property', config?.public.nuxt)
   expect(config?.public).toEqual({
     hello: 'nuxt',
     testValue: 123,
